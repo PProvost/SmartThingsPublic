a57ac2bf2c0cf6f0ec2101a0e4320c626ac98716
PProvost <peter@provost.org>
2017-10-24 19:24:32 -0600

    Update

--- devicetypes/pprovost/ge-plugin-dimmer.src/ge-plugin-dimmer.groovy
+++ devicetypes/pprovost/ge-plugin-dimmer.src/ge-plugin-dimmer.groovy
@@ -23,7 +23,7 @@ metadata {
 		capability "Health Check"
 		capability "Light"
 
-		fingerprint mfr:"0063", prod:"5044", deviceJoinName: "GE Plug-In Smart Dimmer"
+		// fingerprint mfr:"0063", prod:"5044", deviceJoinName: "GE Plug-In Smart Dimmer"
 	}
 
 	simulator {
